256d9308ea59a6120fa91bcebdba1891bdae7884
IrinaVitkovskaya <123252940+IrinaVitkovskaya@users.noreply.github.com>
2024-01-16 19:08:35 +0300

    Add files via upload

--- HW 18 - Hint'ы/hw18_Hint'ы.sql
+++ HW 18 - Hint'ы/hw18_Hint'ы.sql
@@ -23,24 +23,23 @@ GROUP BY ord.CustomerID, det.StockItemID
 ORDER BY ord.CustomerID, det.StockItemID
 
 --Оптимизированный код запроса:
---Условие WHERE для проверки столбца SupplierId было заменено на соединение с таблицей Warehouse.StockItems.
---Условие для сравнения суммы было заменено на оператор EXISTS с подзапросом, который группирует и проверяет сумму.
---Вместо использования функции DATEDIFF сокращенный алиас DAY был использован для читаемости кода.
+--оптимизирован с использованием подзапроса вместо функции DATEDIFF. 
+--присоединение внешней таблицы Warehouse.StockItems происходит непосредственно в основном запросе, вместо использования подзапроса в условии сравнения.
 SELECT ord.CustomerID, det.StockItemID, SUM(det.UnitPrice), SUM(det.Quantity), COUNT(ord.OrderID)
 FROM Sales.Orders AS ord
 JOIN Sales.OrderLines AS det ON det.OrderID = ord.OrderID
-JOIN Sales.Invoices AS Inv ON Inv.OrderID = ord.OrderID
+JOIN Sales.Invoices AS Inv ON Inv.OrderID = ord.OrderID AND Inv.BillToCustomerID != ord.CustomerID
 JOIN Sales.CustomerTransactions AS Trans ON Trans.InvoiceID = Inv.InvoiceID
+JOIN Warehouse.StockItems AS It ON It.StockItemID = det.StockItemID AND It.SupplierId = 12
 JOIN Warehouse.StockItemTransactions AS ItemTrans ON ItemTrans.StockItemID = det.StockItemID
-JOIN Warehouse.StockItems AS It ON It.StockItemID = det.StockItemID
-WHERE Inv.BillToCustomerID != ord.CustomerID
-AND It.SupplierId = 12
-AND EXISTS (SELECT 1
-            FROM Sales.OrderLines AS Total
-            JOIN Sales.Orders AS ordTotal ON ordTotal.OrderID = Total.OrderID
-            WHERE ordTotal.CustomerID = Inv.CustomerID
-            GROUP BY ordTotal.CustomerID
-            HAVING SUM(Total.UnitPrice*Total.Quantity) > 250000)
-AND DATEDIFF(DAY, Inv.InvoiceDate, ord.OrderDate) = 0
+WHERE DATEDIFF(DAY, Inv.InvoiceDate, ord.OrderDate) = 0
+    AND (
+        SELECT SUM(Total.UnitPrice*Total.Quantity)
+        FROM Sales.OrderLines AS Total
+        JOIN Sales.Orders AS ordTotal ON ordTotal.OrderID = Total.OrderID
+        WHERE ordTotal.CustomerID = ord.CustomerID
+        GROUP BY ordTotal.CustomerID
+    ) > 250000
 GROUP BY ord.CustomerID, det.StockItemID
 ORDER BY ord.CustomerID, det.StockItemID
+
